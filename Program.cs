9ab1bda5069461e4112c82d5fcfae0ab39a370e5
Akteur <57916962+Akteur@users.noreply.github.com>
2020-08-14 13:41:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using FakerLib;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string reg = "";
+            int count = 0;
+            double errCount = 0;
+            IFormatProvider formatter 
+                = new NumberFormatInfo { NumberDecimalSeparator = "." };
+            if (args.Length > 0)
+            {
+                if (args.Length >= 3)
+                {
+                    if (int.TryParse(args[1], out count) && double.TryParse(args[2], NumberStyles.Any, CultureInfo.InvariantCulture, out errCount)
+                        && Int32.Parse(args[1]) >= 0 && double.Parse(args[2], formatter) >= 0)
+                    {
+                        
+                        reg = args[0];
+                        count = Int32.Parse(args[1]);
+                        errCount = double.Parse(args[2], formatter);
+                        double peace_errCount = errCount - Math.Floor(errCount);
+                        Person person = new Person(reg, count, errCount, peace_errCount);
+
+                        person.PersonGen();
+                        person.PersonOutput();
+                    }
+                    else
+                    {
+                        Console.Error.WriteLine("\nERROR:   Invalid arguments passed");
+                    }
+                }
+                else
+                {
+                    reg = args[0];
+                    count = Int32.Parse(args[1]);
+                    errCount = 0;
+                    double peace_errCount = 0;
+                    Person person = new Person(reg, count, errCount, peace_errCount);
+                    person.PersonGen();
+                    person.PersonOutput();
+                }
+            }
+            else
+            {
+                Console.Error.WriteLine("\nERROR:   Programm need arguments.");
+            }
+        }
+    }
+}
